c210746793106ed8260749fd22b1f520b892e7ec
Bhavik Baraiya <bhavik@mobiuso.com>
2025-03-22 10:58:43 +0530

    Initial Commit

---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Bhavik Baraiya on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeBookTests/RecipeBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookTests.swift
+//  RecipeBookTests
+//
+//  Created by Bhavik Baraiya on 22/03/25.
+//
+
+import Testing
+@testable import RecipeBook
+
+struct RecipeBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
