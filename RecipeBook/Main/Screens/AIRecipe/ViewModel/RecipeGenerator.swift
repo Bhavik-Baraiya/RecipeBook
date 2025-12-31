29cf42315cfed4317c509890b7f68a613a3e7118
Bhavik Baraiya <bhavik@mobiuso.com>
2025-12-31 19:51:08 +0530

    Add AIRecipe option on Tabbar navigation.

---
+++ RecipeBook/Main/Screens/AIRecipe/ViewModel/RecipeGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeGenerator.swift
+//  RecipeBook
+//
+//  Created by Bhavik Baraiya on 31/12/25.
+//
+
+import SwiftUI
+
+@Observable
+class RecipeGenerator {
+    var AIRecipeInput: String = ""
+}
--- RecipeBook/MainView.swift
+++ RecipeBook/MainView.swift
@@ -10,34 +10,48 @@ import SwiftUI
 struct MainView: View {
     
     @State private var selectedTab = 0
+    private var isAppleInteligenceAvailable: Bool = true
     
     var body: some View {
         
         TabView(selection: $selectedTab) {
-            
-            RecipeListView()
+            configureTabView()
+        } //: TABVIEW
+    }
+    
+    @ViewBuilder
+    func configureTabView() -> some View {
+        RecipeListView()
+            .tabItem {
+                Image(selectedTab == 0 ? "recipes-highlighted" : "recipes-plain")
+                Text("My Recipe")
+                    .foregroundStyle(.primaryApp)
+                
+            }.tag(0)
+        
+        if(isAppleInteligenceAvailable) {
+            AIRecipeView()
                 .tabItem {
-                    Image(selectedTab == 0 ? "recipes-highlighted" : "recipes-plain")
-                    Text("My Recipe")
+                    Image(selectedTab == 1 ? "recipe-ai-highlighted" : "recipe-ai-plain")
+                    Text("AI Recipe")
                         .foregroundStyle(.primaryApp)
                     
-                }.tag(0)
-
-            FavouriteRecipeListView()
-                .tabItem {
-                    Image(selectedTab == 1 ? "favourite-highlighted" : "favourite-plain")
-                    Text("Favourites")
-                        .foregroundStyle(.primaryApp)
                 }.tag(1)
+        }
+        
+        FavouriteRecipeListView()
+            .tabItem {
+                Image(selectedTab == 2 ? "favourite-highlighted" : "favourite-plain")
+                Text("Favourites")
+                    .foregroundStyle(.primaryApp)
+            }.tag(2)
 
-            SettingsView()
-                .tabItem {
-                    Image(selectedTab == 2 ? "settings-highlighted" : "settings-plain")
-                    Text("Settings")
-                        .foregroundStyle(.primaryApp)
-                }.tag(2)
-            
-        } //: TABVIEW
+        SettingsView()
+            .tabItem {
+                Image(selectedTab == 3 ? "settings-highlighted" : "settings-plain")
+                Text("Settings")
+                    .foregroundStyle(.primaryApp)
+            }.tag(3)
     }
 }
 
